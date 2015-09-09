5a3848db3920950a73ba4838229b7aea935463ec
Vito Van <awesomevito@live.com>
2015-09-09 09:20:00 +0800

    What? Clack doesn't support hot deployment?

--- spider.lisp
+++ spider.lisp
@@ -1,9 +1,9 @@
 (setf sb-impl::*default-external-format* :UTF-8)
 ;;(declaim (optimize (debug 3)))
-(ql:quickload '(drakma plump clss clack clack-v1-compat ningle cl-json clack-errors))
+(ql:quickload '(drakma plump clss clack clack-v1-compat cl-json))
 
 (defpackage cl-spider
-  (:use :cl :drakma :plump :clss :clack :ningle :json))
+  (:use :cl :drakma :plump :clss :clack :json))
 (in-package :cl-spider)
 
 (defun get-html (uri &key (expected-code 200) (method :get) parameters)
@@ -23,14 +23,11 @@
 (defun get-text(node)
   (string-trim '(#\Space #\Tab #\Newline) (text node)))
 
-(defun get-attribute(attr node)
-  (attribute node attr))
-
 (defun get-what-I-want (uri selector &key attr)
   (map 'list
        #'(lambda (node)
            (if attr
-               (get-attribute attr node)
+               (attribute node attr)
                (get-text node)))
        (get-nodes selector (get-dom (get-html uri)))))
 
@@ -51,53 +48,26 @@
 ;;(get-multiple-what-i-want
 ;;  '("http://sh.58.com/xiaoqu/xqlist_A_1/" "http://sh.58.com/xiaoqu/xqlist_B_1/") "ul>li>a" :attr "href")
 
-(defvar *app* (make-instance 'ningle:<app>))
+(defvar *env* nil)
 
-(setf (route *app* "/")
-      #'(lambda (params)
-          (setf (lack.response:response-headers *response*) '(:content-type "text/html"))
-          "Welcome to  Such Cute!"))
-
-(defmacro set-content-type (content-type)
-  `(setf (lack.response:response-headers *response*) '(:content-type ,content-type)))
-
-(defmacro get-param-value(key)
-  `(cdr (assoc ,key params :test #'string=)))
-
-(defun test-doge (uri selector attr)
-  (get-what-i-want uri selector :attr attr))
-
-(setf (route *app* "/doge" :method :POST)
-      #'(lambda (params)
-          (set-content-type "application/json")
-          (let* ((action (get-param-value "action"))
-                 (uri (get-param-value "uri"))
-                 (selector (get-param-value "selector"))
-                 (attr (get-param-value "attr")))
-            (cond
-              ((equal action "test")
-               (encode-json-to-string (test-doge uri selector attr)))
-              ((equal action "fuck") (format nil "~A" (get-param-value "name")))))))
-
-(setf (route *app* "/doge" :method :GET)
-      #'(lambda (params)
-          (set-content-type "application/json")
-          (encode-json-to-string params)))
-
-(setf (route *app* "/dogep" :method :GET)
-      #'(lambda (params)
-          (set-content-type "application/javascript")
-          (encode-json-to-string params)))
+(defun app (env)
+  (setf *env* env)
+  (cond
+    ((string= (getf env :path-info) "/")
+     '(200 (:content-type "text/html") ("Hello fucker")))
+    ((string= (getf env :path-info) "/doge")
+     `(200 (:content-type "application/json" :fucker "hey")
+           (,(format nil "~A" env))))
+    (t
+     '(404 (:content-type "text/plain") ("Not found")))))
 
 (defvar *handler*)
 
-(defun start ()
+(defun start-server ()
   (setf *handler*
-        (clack:clackup
-         (funcall clack-errors:*clack-error-middleware*
-                  *app*
-                  :debug t)
-         :port 5000
-         :server :hunchentoot)))
+        (clackup
+         #'app
+         :server :woo)))
 
-(start)
+(defun stop-server ()
+  (stop *handler*))
